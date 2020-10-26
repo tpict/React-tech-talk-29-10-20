e418be79f71791f293584e08ceaee7c899917abb
Tom Picton <tom@tompicton.com>
2020-10-26 12:18:11 -0700

    2. Props demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+import chocChipImage from "./chocChip.png";
+import doubleChocChipImage from "./doubleChocChip.png";
+
+export const Cookie = function (props) {
+  const alt =
+    props.variant === "chocChip"
+      ? "Chocolate chip cookie"
+      : "Double chocolate chip cookie";
+
+  const src =
+    props.variant === "chocChip" ? chocChipImage : doubleChocChipImage;
+
+  return React.createElement("img", {
+    alt,
+    src,
+  });
+};
+
 export const App = function () {
-  return React.createElement("div", { className: "App" }, ["Hello world"]);
+  return React.createElement("div", { className: "App" }, [
+    React.createElement("div", { className: "cookie-decor" }, [
+      React.createElement(Cookie, { variant: "chocChip" }),
+      React.createElement(Cookie, { variant: "doubleChocChip" }),
+    ]),
+    "Hello world",
+  ]);
 };
